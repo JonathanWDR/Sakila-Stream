f6aca00ae82897dbf10d5258f48260e2efb76684
Jonathan Wieder <33692859+JonathanWDR@users.noreply.github.com>
2025-07-07 15:57:29 +0200

    Create 05_Drop.sql

---
+++ JAN_TARNBIR_JONATHAN/05_Drop.sql
@@ -0,0 +1,10 @@
+SET search_path TO sakila;
+
+
+Drop table if exists staff cascade;
+
+drop table if exists inventory cascade;
+
+drop table if exists store cascade;
+
+drop table if exists rental cascade;
