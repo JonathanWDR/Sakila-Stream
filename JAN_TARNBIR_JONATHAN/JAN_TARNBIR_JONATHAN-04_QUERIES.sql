9ad04fdb25f06d47b40977f62d65d5a509f366e5
Jonathan Wieder <33692859+JonathanWDR@users.noreply.github.com>
2025-07-12 22:42:25 +0200

    Update JAN_TARNBIR_JONATHAN-04_QUERIES.sql

--- JAN_TARNBIR_JONATHAN/JAN_TARNBIR_JONATHAN-04_QUERIES.sql
+++ JAN_TARNBIR_JONATHAN/JAN_TARNBIR_JONATHAN-04_QUERIES.sql
@@ -1,7 +1,19 @@
--- Create new Category: Dramedy
+-- 2.4.4 Zusätzliche Daten einfügen: Neue Kategorie 'Dramedy'
+
+-- a. Neue Kategorie 'Dramedy' anlegen
 INSERT INTO category (category_id, name, last_update)
 VALUES (
-  (SELECT COALESCE(MAX(category_id), 0) + 1 FROM category),
-  'Dramedy',
-  now()
+    (SELECT COALESCE(MAX(category_id), 0) + 1 FROM category),
+    'Dramedy', 
+    CURRENT_TIMESTAMP
 );
+
+-- b. Neue Kategorie allen Inhalten hinzufügen, die gleichzeitig den Kategorien "Drama" und "Comedy" zugeordnet sind
+INSERT INTO content_category (content_id, category_id)
+SELECT DISTINCT 
+    cc1.content_id,
+    (SELECT category_id FROM category WHERE name = 'Dramedy') AS category_id
+FROM content_category cc1
+JOIN content_category cc2 ON cc1.content_id = cc2.content_id
+WHERE cc1.category_id = (SELECT category_id FROM category WHERE name = 'Drama')
+  AND cc2.category_id = (SELECT category_id FROM category WHERE name = 'Comedy');
