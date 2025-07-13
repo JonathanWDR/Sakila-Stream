57eb16cb28ad1bbf9c0e14864d1291b96bddf175
Jan <jan.mueller02@sap.com>
2025-07-13 20:31:31 +0200

    automatic naming conventions for constraints

--- JAN_TARNBIR_JONATHAN/JAN_TARNBIR_JONATHAN-01_CREATE.sql
+++ JAN_TARNBIR_JONATHAN/JAN_TARNBIR_JONATHAN-01_CREATE.sql
@@ -26,7 +26,7 @@ CREATE TABLE content_type (
   content_type_id SMALLINT    NOT NULL,
   content_ty_name VARCHAR(64) NOT NULL,
   additional_info VARCHAR(255),
-  CONSTRAINT pk_content_type PRIMARY KEY (content_type_id)
+  PRIMARY KEY (content_type_id)
 );
 
 
@@ -58,7 +58,7 @@ CREATE TABLE content_actor (
 CREATE TABLE content_category (
    content_id INTEGER NOT NULL,
    category_id SMALLINT NOT NULL,
-   CONSTRAINT pk_category_id PRIMARY KEY (content_id, category_id),
+   PRIMARY KEY (content_id, category_id),
    FOREIGN KEY (content_id) REFERENCES content_stream(content_id),
    FOREIGN KEY (category_id) REFERENCES category(category_id)
 );
@@ -85,9 +85,8 @@ CREATE TABLE franchise (
   start_content_id    INTEGER,
   franchise_name      VARCHAR(80)  NOT NULL,
   franchise_descr     VARCHAR(255) NOT NULL,
-  CONSTRAINT pk_franchise PRIMARY KEY (franchise_id),
-  CONSTRAINT fk_franchise_start_content
-    FOREIGN KEY (start_content_id)
+  PRIMARY KEY (franchise_id),
+  FOREIGN KEY (start_content_id)
       REFERENCES content_stream (content_id)
       ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -99,15 +98,13 @@ CREATE INDEX idx_franchise_fk_start_content_id
 CREATE TABLE content_language (
   content_id   INTEGER   NOT NULL,
   language_id  SMALLINT  NOT NULL,
-  CONSTRAINT pk_content_language PRIMARY KEY (content_id, language_id),
-  CONSTRAINT fk_content_language_content
-    FOREIGN KEY (content_id)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_content_language_language
-    FOREIGN KEY (language_id)
-      REFERENCES language (language_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (content_id, language_id),
+  FOREIGN KEY (content_id)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (language_id)
+    REFERENCES language (language_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_content_language_fk_language_id
@@ -118,13 +115,13 @@ CREATE TABLE video_quality (
   video_quality_id  SMALLINT      NOT NULL,
   vidquality_label  VARCHAR(5)    NOT NULL,
   vidquality_descr  VARCHAR(40)   NOT NULL,
-  CONSTRAINT pk_video_quality PRIMARY KEY (video_quality_id)
+  PRIMARY KEY (video_quality_id)
 );
 
 CREATE TABLE service_type (
   service_type_id    SMALLINT   NOT NULL,
   service_type_name  VARCHAR(128) NOT NULL,
-  CONSTRAINT pk_service_type PRIMARY KEY (service_type_id)
+  PRIMARY KEY (service_type_id)
 );
 
 
@@ -132,15 +129,13 @@ CREATE TABLE video_quality_price (
   service_type_id    SMALLINT     NOT NULL,
   video_quality_id   SMALLINT     NOT NULL,
   vid_quality_price  DECIMAL(4,2) NOT NULL,
-  CONSTRAINT pk_video_quality_price PRIMARY KEY (service_type_id, video_quality_id),
-  CONSTRAINT fk_vqp_service_type
-    FOREIGN KEY (service_type_id)
-      REFERENCES service_type (service_type_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_vqp_video_quality
-    FOREIGN KEY (video_quality_id)
-      REFERENCES video_quality (video_quality_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (service_type_id, video_quality_id),
+  FOREIGN KEY (service_type_id)
+    REFERENCES service_type (service_type_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (video_quality_id)
+    REFERENCES video_quality (video_quality_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_vqp_fk_video_quality_id
@@ -152,11 +147,10 @@ CREATE TABLE package (
   category_id      SMALLINT     NOT NULL,
   additional_info  VARCHAR(255),
   price            DECIMAL(4,2) NOT NULL,
-  CONSTRAINT pk_package PRIMARY KEY (package_id),
-  CONSTRAINT fk_package_category
-    FOREIGN KEY (category_id)
-      REFERENCES category (category_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (package_id),
+  FOREIGN KEY (category_id)
+    REFERENCES category (category_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_package_fk_category_id
@@ -168,7 +162,7 @@ CREATE TABLE subscription (
   subscr_name      VARCHAR(128) NOT NULL,
   additional_info  VARCHAR(255),
   price            DECIMAL(4,2) NOT NULL,
-  CONSTRAINT pk_subscription PRIMARY KEY (subscr_id)
+  PRIMARY KEY (subscr_id)
 );
 
 
@@ -182,19 +176,16 @@ CREATE TABLE srv_customer_allocation (
   end_date           TIMESTAMP,
   active             BOOLEAN     NOT NULL DEFAULT TRUE,
   last_update        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_srv_customer_allocation PRIMARY KEY (srv_cust_alloc_id),
-  CONSTRAINT fk_sca_service_type
-    FOREIGN KEY (service_type_id)
-      REFERENCES service_type (service_type_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_sca_video_quality
-    FOREIGN KEY (video_quality)
-      REFERENCES video_quality (video_quality_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_sca_customer
-    FOREIGN KEY (customer_id)
-      REFERENCES customer (customer_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
+  PRIMARY KEY (srv_cust_alloc_id),
+  FOREIGN KEY (service_type_id)
+    REFERENCES service_type (service_type_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (video_quality)
+    REFERENCES video_quality (video_quality_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (customer_id)
+    REFERENCES customer (customer_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT chk_valid_service_type 
     CHECK (service_type_id IN (1, 2, 3))
 );
@@ -265,11 +256,10 @@ CREATE TABLE billing_head (
   customer_id   INTEGER    NOT NULL,
   billing_date  DATE       NOT NULL,
   last_update   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_billing_head PRIMARY KEY (billing_id),
-  CONSTRAINT fk_billing_head_customer
-    FOREIGN KEY (customer_id)
-      REFERENCES customer (customer_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (billing_id),
+  FOREIGN KEY (customer_id)
+    REFERENCES customer (customer_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_billing_head_fk_customer_id
@@ -283,19 +273,16 @@ CREATE TABLE billing_item (
   service_type_id    SMALLINT    NOT NULL,
   amount             DECIMAL(5,2) NOT NULL,
   last_update        TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_billing_item PRIMARY KEY (billing_id, billing_item_id),
-  CONSTRAINT fk_bitem_billing_head
-    FOREIGN KEY (billing_id)
-      REFERENCES billing_head (billing_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_bitem_sca
-    FOREIGN KEY (srv_cust_alloc_id)
-      REFERENCES srv_customer_allocation (srv_cust_alloc_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_bitem_service_type
-    FOREIGN KEY (service_type_id)
-      REFERENCES service_type (service_type_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (billing_id, billing_item_id),
+  FOREIGN KEY (billing_id)
+    REFERENCES billing_head (billing_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (srv_cust_alloc_id)
+    REFERENCES srv_customer_allocation (srv_cust_alloc_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (service_type_id)
+    REFERENCES service_type (service_type_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_billing_item_fk_sca_id
@@ -309,9 +296,8 @@ CREATE INDEX idx_billing_item_fk_service_type_id
 
 ALTER TABLE payment
 ADD column billing_id bigint,
-ADD CONSTRAINT fk_billing_id
-FOREIGN KEY (billing_id) REFERENCES billing_head(billing_id)
-ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD FOREIGN KEY (billing_id) REFERENCES billing_head(billing_id)
+  ON DELETE RESTRICT ON UPDATE CASCADE;
 
 ALTER TABLE payment 
 DROP CONSTRAINT pk_payment;
@@ -328,15 +314,13 @@ ADD CONSTRAINT pk_payment PRIMARY KEY (payment_id);
 CREATE TABLE package_content (
   package_id  SMALLINT  NOT NULL,
   content_id  INTEGER   NOT NULL,
-  CONSTRAINT pk_package_content PRIMARY KEY (package_id, content_id),
-  CONSTRAINT fk_pkgcontent_package
-    FOREIGN KEY (package_id)
-      REFERENCES package (package_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_pkgcontent_content
-    FOREIGN KEY (content_id)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (package_id, content_id),
+  FOREIGN KEY (package_id)
+    REFERENCES package (package_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (content_id)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_package_content_fk_content_id
@@ -350,11 +334,10 @@ CREATE TABLE customer_watchlist (
   name           VARCHAR(25) NOT NULL,
   description    VARCHAR(255),
   last_update    TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_customer_watchlist PRIMARY KEY (watchlist_id),
-  CONSTRAINT fk_customer_watchlist_customer
-    FOREIGN KEY (customer_id)
-      REFERENCES customer (customer_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (watchlist_id),
+  FOREIGN KEY (customer_id)
+    REFERENCES customer (customer_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_customer_watchlist_fk_customer_id
@@ -366,15 +349,13 @@ CREATE TABLE cust_watchlist_item (
   content_id    INTEGER    NOT NULL,
   seq_num       SMALLINT   NOT NULL,
   last_update   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_cust_watchlist_item PRIMARY KEY (watchlist_id, content_id),
-  CONSTRAINT fk_cust_watchlist_item_watchlist
-    FOREIGN KEY (watchlist_id)
-      REFERENCES customer_watchlist (watchlist_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_cust_watchlist_item_content
-    FOREIGN KEY (content_id)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (watchlist_id, content_id),
+  FOREIGN KEY (watchlist_id)
+    REFERENCES customer_watchlist (watchlist_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (content_id)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_cust_watchlist_item_fk_content_id
@@ -389,15 +370,13 @@ CREATE TABLE cust_watch_act (
   time_index_seconds SMALLINT,
   completion_date    TIMESTAMP,
   last_update        TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT pk_cust_watch_act PRIMARY KEY (cust_watch_act_id),
-  CONSTRAINT fk_cust_watch_act_customer
-    FOREIGN KEY (customer_id)
-      REFERENCES customer (customer_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_cust_watch_act_content
-    FOREIGN KEY (content_id)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (cust_watch_act_id),
+  FOREIGN KEY (customer_id)
+    REFERENCES customer (customer_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (content_id)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_cust_watch_act_fk_customer_id
@@ -410,15 +389,13 @@ CREATE INDEX idx_cust_watch_act_fk_content_id
 CREATE TABLE content_country_restricted (
   country_id INTEGER NOT NULL,
   content_id INTEGER  NOT NULL,
-  CONSTRAINT pk_content_country_restricted PRIMARY KEY (country_id, content_id),
-  CONSTRAINT fk_ccr_country
-    FOREIGN KEY (country_id)
-      REFERENCES country (country_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_ccr_content
-    FOREIGN KEY (content_id)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (country_id, content_id),
+  FOREIGN KEY (country_id)
+    REFERENCES country (country_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (content_id)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE INDEX idx_ccr_fk_content_id
@@ -429,19 +406,16 @@ CREATE TABLE binge_flow (
   current_id     INTEGER   NOT NULL,
   next_content   INTEGER   NOT NULL,
   franchise_id   SMALLINT  NOT NULL,
-  CONSTRAINT pk_binge_flow PRIMARY KEY (current_id, next_content),
-  CONSTRAINT fk_binge_flow_current
-    FOREIGN KEY (current_id)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_binge_flow_next
-    FOREIGN KEY (next_content)
-      REFERENCES content_stream (content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_binge_flow_franchise
-    FOREIGN KEY (franchise_id)
-      REFERENCES franchise (franchise_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  PRIMARY KEY (current_id, next_content),
+  FOREIGN KEY (current_id)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (next_content)
+    REFERENCES content_stream (content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (franchise_id)
+    REFERENCES franchise (franchise_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 
@@ -457,15 +431,13 @@ CREATE INDEX idx_binge_flow_fk_franchise_id
 CREATE TABLE series (
   series_id     INTEGER     PRIMARY KEY,
   content_id    INTEGER     NOT NULL,
-  franchise_id  SMALLINT    NOT NULL,
-  CONSTRAINT fk_series_content   
-    FOREIGN KEY (content_id)   
-      REFERENCES content_stream(content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_series_franchise
-    FOREIGN KEY (franchise_id)
-      REFERENCES franchise(franchise_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  franchise_id  SMALLINT    NOT NULL,   
+  FOREIGN KEY (content_id)   
+    REFERENCES content_stream(content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (franchise_id)
+    REFERENCES franchise(franchise_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE season (
@@ -473,14 +445,12 @@ CREATE TABLE season (
   series_id      INTEGER   NOT NULL,
   content_id     INTEGER   NOT NULL,
   season_number  SMALLINT  NOT NULL,
-  CONSTRAINT fk_season_series
-    FOREIGN KEY (series_id)
-      REFERENCES series(series_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_season_content
-    FOREIGN KEY (content_id)
-      REFERENCES content_stream(content_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE
+  FOREIGN KEY (series_id)
+    REFERENCES series(series_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (content_id)
+    REFERENCES content_stream(content_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE episode (
@@ -488,12 +458,10 @@ CREATE TABLE episode (
   season_id      INTEGER   NOT NULL,
   content_id     INTEGER   NOT NULL,
   episode_number SMALLINT  NOT NULL,
-  CONSTRAINT fk_episode_season
-    FOREIGN KEY (season_id)
-      REFERENCES season(season_id)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT fk_episode_content
-    FOREIGN KEY (content_id)
-    REFERENCES content_stream(content_id)
-    ON DELETE RESTRICT ON UPDATE CASCADE
+  FOREIGN KEY (season_id)
+    REFERENCES season(season_id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (content_id)
+  REFERENCES content_stream(content_id)
+  ON DELETE RESTRICT ON UPDATE CASCADE
 );
